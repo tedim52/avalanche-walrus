c7c682a01952a3e792ec2187b07e27239de4abfe
Quin Burke <burkequin@gmail.com>
2022-10-23 17:38:25 -0400

    Improve Testbed to Automate Infrastructure Automates adding nodes to network, funding the network, and adding clients to each node

---
+++ testbed/go.mod
@@ -0,0 +1,34 @@
+module testbed
+
+go 1.19
+
+require (
+	github.com/ava-labs/coreth v0.11.0
+	github.com/ethereum/go-ethereum v1.10.25
+)
+
+require (
+	github.com/ava-labs/avalanchego v1.8.5 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.2.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ testbed/main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ava-labs/coreth/ethclient"
+)
+
+func post(url string, payload string) string {
+	request, error := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
+	if error != nil {
+		panic(error)
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	//fmt.Println("response Status:", response.Status)
+	//fmt.Println("response Headers:", response.Header)
+	body, _ := ioutil.ReadAll(response.Body)
+	//fmt.Println("response Body:", string(body))
+	return string(body)
+}
+
+// TODO add nodes method
+func start_nodes(n int, path string) {
+	httpposturl := "http://localhost:8081/v1/control/start"
+
+	var jsonData = fmt.Sprintf(`{
+		"execPath": "%s",
+		"numNodes": "%d",
+		"logLevel":"INFO"
+	}`, path, n)
+	post(httpposturl, jsonData)
+}
+
+func add_node(x int, path string) {
+	post("http://localhost:8081/v1/control/addnode", fmt.Sprintf(`{"name":"node%d","execPath":"%s","logLevel":"INFO"}`, x, path))
+}
+
+func pollNode(addr string) {
+	client, err := ethclient.Dial(fmt.Sprintf("ws://%s/ext/bc/C/ws", addr))
+	for err != nil {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Retrying %s\n", addr)
+		client, err = ethclient.Dial(fmt.Sprintf("ws://%s/ext/bc/C/ws", addr))
+	}
+
+	hash_channel := make(chan *common.Hash)
+
+	sub, err := client.SubscribeNewAcceptedTransactions(context.Background(), hash_channel)
+	if err != nil {
+		fmt.Println("sub failed", err)
+	}
+
+    fmt.Printf("Listening on %s\n", addr) 
+	for {
+	  select {
+  	    case err := <-sub.Err():
+    	      fmt.Println(err)
+  	    case vLog := <-hash_channel:
+    	      //fmt.Println(vLog) // pointer to event log
+	      tx, _, err := client.TransactionByHash(context.Background(), *vLog) 
+	      if err != nil {
+		fmt.Println("tx lookup failed", err)
+	      } else {
+		fmt.Println(vLog, " => ", tx.FirstSeen())
+	      }
+      	}
+	}
+}
+
+func checkHealth() bool {
+	res := post("http://localhost:8081/v1/control/status", "")
+	var result map[string]any
+	json.Unmarshal([]byte(res), &result)
+	clusterInfo := result["clusterInfo"].(map[string]any)
+	health := clusterInfo["healthy"].(bool)
+	return health
+}
+
+func getAddrs(n int) []string {
+	res := post("http://localhost:8081/v1/control/status", "")
+	var result map[string]any
+	json.Unmarshal([]byte(res), &result)
+	nodeInfo, ok := result["clusterInfo"].(map[string]any)["nodeInfos"].(map[string]any)
+	if !ok {
+		fmt.Println(result)
+		panic("No nodeInfos")
+	}
+	addrs := make([]string, n)
+	i := 0
+	prefix := len("http://")
+	for _, value := range nodeInfo {
+		uri := value.(map[string]any)["uri"].(string)
+		addrs[i] = uri[prefix:]
+		i++
+	}
+	return addrs
+}
+
+func fundNetwork() {
+	pword := "YY0*id@K#A29"
+	// Create User
+	url := "http://127.0.0.1:9650/ext/keystore"
+	payload := fmt.Sprintf(`{
+		"jsonrpc":"2.0",
+		"id"     :1,
+		"method" :"keystore.createUser",
+		"params" :{
+			"username": "username",
+			"password": "%s"
+		}
+		`, pword)
+	post(url, payload)
+	
+	// Import Key
+	url = "http://127.0.0.1:9650/ext/bc/X"
+	payload = fmt.Sprintf(`{
+		"jsonrpc":"2.0",
+		"id"     :1,
+		"method" :"avm.importKey",
+		"params" :{
+			"username": "username",
+			"password": "%s",
+			"privateKey":"PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"
+		}
+		`, pword)
+	post(url, payload)
+	
+	// Check Balance
+	url = "http://127.0.0.1:9650/ext/bc/X"
+	payload = `{
+		"jsonrpc":"2.0",
+		"id"     : 1,
+		"method" :"avm.getBalance",
+		"params" :{
+			"address":"X-custom18jma8ppw3nhx5r4ap8clazz0dps7rv5u9xde7p",
+			"assetID": "AVAX"
+		}
+	}`
+	post(url, payload)
+}
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+        log.Fatalf("failed to open")
+    }
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+    
+	total_nodes := 0
+
+    for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+        nodes, _ := strconv.Atoi(line[0])
+		if total_nodes == 0 { // First time do batch launch
+			total_nodes += nodes
+			start_nodes(nodes, line[1])
+		} else { // Add nodes one by one after (no batch available)
+			for i := 0; i < nodes; i++ {
+				total_nodes += 1
+				add_node(total_nodes, line[1])
+			}
+		}
+    }
+
+	file.Close()
+
+	fmt.Print("Waiting for cluster health...")
+	for !checkHealth() {
+		time.Sleep(5 * time.Second)
+		fmt.Print(".")
+	}
+	fmt.Println("")
+
+	// Get Addrs
+	addrs := getAddrs(total_nodes)
+	
+	fmt.Println("Funding Network")
+	fundNetwork()
+
+	// double check port info
+	for _, addr := range addrs {
+        go pollNode(addr)
+    }
+
+	for {}
+}
