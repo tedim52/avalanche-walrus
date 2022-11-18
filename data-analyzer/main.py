42f8c3f8c9954006d08c72e12575a4e6ea0f5bbe
Quin Burke <burkequin@gmail.com>
2022-11-18 17:12:53 -0500

    Plot tx RTT in histogram separated by node

---
+++ data-analyzer/main.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import math
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py path-to-csv")
+        exit(1)
+
+    # Assumes csv well formatted
+    # TODO may want to do some checks
+    df = pd.read_csv(sys.argv[1])
+
+    # Create RTT column
+    rtt_part = {}
+    for ind in df.index:
+        node=df['NodeID'][ind]
+        if not node in rtt_part:
+            rtt_part[node] = []
+        rtt_part[node].append(df['End'][ind] - df['Start'][ind])
+
+    plt.title("RTT of Transactions")
+    plt.xlabel("RTT (ms)")
+    plt.ylabel("# of Tx")
+    plt.hist(list(rtt_part.values()), 8, stacked=True)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
